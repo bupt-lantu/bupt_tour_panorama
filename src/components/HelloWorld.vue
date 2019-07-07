bab4ce936e320e22f6e930d2c09ac811cbd7921f
Al0ha0e <1919270783@qq.com>
2019-07-08 00:42:31 +0800

    add data & clear & preload

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,7 @@
 <template>
   <div>
     <div id="sceneContainer" width="100%" height="100%">
-      {{this.eularAngle.x}}
-      {{this.eularAngle.y}}
-      <button
-        v-if="touchMode"
-        v-on:click="touchMode=false"
-      >切换至重力感应</button>
+      <button id="modebutton" v-if="touchMode" v-on:click="touchMode=false">切换至重力感应</button>
       <button v-else v-on:click="touchMode=true">恢复手动控制</button>
       <audio id="desc" />
     </div>
@@ -15,7 +10,7 @@
 
 <script>
 import * as THREE from "three";
-import { Matrix4 } from "three";
+import { Matrix4, PlaneGeometry } from "three";
 import orientHandler from "./scripts/orienter.js";
 import sceneObj from "./scripts/scene";
 import { log } from "util";
@@ -26,7 +21,9 @@ export default {
       sceneMap: {},
       currentScene: "",
       skyBoxMat: {},
+      preSkyBoxMat: null,
       skyBoxTexture: {},
+      preSkyBoxTexture: null,
       skyBoxGeom: {},
       touchMode: true,
       skyboxReady: false,
@@ -38,7 +35,7 @@ export default {
       mouseDown: false,
       touchDown: false,
       mouseSpeed: 0.1,
-      touchSpeed: 0.1,
+      touchSpeed: 0.3,
       pre: { x: 0, y: 0 },
       eularAngle: { x: 0, y: 0 },
       deviceOrientationData: {
@@ -55,49 +52,45 @@ export default {
   },
   methods: {
     sceneObjInit: function() {
-      let img1 = "https://i.loli.net/2019/05/20/5ce24006dcc9389408.jpg";
-      let img2 = "https://i.loli.net/2019/05/20/5ce24006dcf7e51423.jpg";
-      let aud =
-        "https://langdu.cdn.bjadks.com/DataFile/Upload/User/Audio/20190528143945_45556.mp3";
-      let scene1 = new sceneObj("cross", img1);
-      let scene2 = new sceneObj("library", img2);
-      let cubeobj = new THREE.Mesh(
-        new THREE.BoxGeometry(1, 1, 1),
-        new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-      );
-      cubeobj.position.x = cubeobj.position.y = 3;
-      scene1.addJumpObj(cubeobj, "library");
-      let sphereobj = new THREE.Mesh(
-        new THREE.SphereGeometry(0.5, 10, 10),
-        new THREE.MeshBasicMaterial({ color: 0xffffff })
-      );
-      sphereobj.position.x = 10;
-      sphereobj.position.y = 2.5;
-      sphereobj.position.z = -10;
-      let sphereobj2 = new THREE.Mesh(
-        new THREE.SphereGeometry(4, 10, 10),
-        new THREE.MeshBasicMaterial({
-          color: 0xffffff,
-          opacity: 0,
-          transparent: true
-        })
-      );
-      sphereobj2.position.x = 10;
-      sphereobj2.position.y = 2.5;
-      sphereobj2.position.z = -10;
-      scene2.addNormalObj(sphereobj);
-      scene2.addDescObj(sphereobj2, aud);
-      this.currentScene = scene1;
+      let img1 = "https://i.loli.net/2019/07/07/5d21fc2e85ed526354.jpg"; //图书馆和教学楼间//"https://i.loli.net/2019/05/20/5ce24006dcc9389408.jpg";
+      let img2 = "https://i.loli.net/2019/07/07/5d21fc2e85f6c39262.jpg"; //靠近图书馆"https://i.loli.net/2019/05/20/5ce24006dcf7e51423.jpg";
+      let img3 = "https://i.loli.net/2019/07/07/5d21fc2e85e0554551.jpg"; //教室地下
+      let img4 = "https://i.loli.net/2019/07/07/5d21fc2e85f8953320.jpg"; //食堂连廊
+      let img5 = "https://i.loli.net/2019/07/07/5d21fc2e85c4d50768.jpg"; //食堂路口
+      let img1p = "https://i.loli.net/2019/07/07/5d22104ea845884854.jpg"; //图书馆和教学楼间
+      let img2p = "https://i.loli.net/2019/07/07/5d22104ec7b3415917.jpg"; //靠近图书馆 "https://i.loli.net/2019/07/07/5d21fef1c332f75397.jpg"; //
+      let img3p = "https://i.loli.net/2019/07/07/5d22104ebdcf286019.jpg"; //教室地下
+      let img4p = "https://i.loli.net/2019/07/07/5d22104eb37e563108.jpg"; //食堂连廊
+      let img5p = "https://i.loli.net/2019/07/07/5d22104e964f185346.jpg"; //食堂路口
+      //let aud =
+      //  "https://langdu.cdn.bjadks.com/DataFile/Upload/User/Audio/20190528143945_45556.mp3";
+      let pi = Math.PI;
+      let scene1 = new sceneObj("图书馆和教学楼间", img1, img1p);
+      scene1.addJumpObj(this.getJumpButton(pi / 2, 10, 1), "靠近图书馆");
+      scene1.addJumpObj(this.getJumpButton(-pi / 2, 10, 1), "教室地下");
+      let scene2 = new sceneObj("靠近图书馆", img2, img2p);
+      scene2.addJumpObj(this.getJumpButton(-pi / 2, 10, 1), "图书馆和教学楼间");
+      scene2.addJumpObj(this.getJumpButton(pi / 2, 10, 1), "食堂连廊");
+      let scene3 = new sceneObj("教室地下", img3, img3p);
+      scene3.addJumpObj(this.getJumpButton(-pi / 2, 10, 1), "图书馆和教学楼间");
+      let scene4 = new sceneObj("食堂连廊", img4, img4p);
+      scene4.addJumpObj(this.getJumpButton(0, 10, 1), "靠近图书馆");
+      scene4.addJumpObj(this.getJumpButton(pi, 10, 1), "食堂路口");
+      let scene5 = new sceneObj("食堂路口", img5, img5p);
+      scene5.addJumpObj(this.getJumpButton((6 * pi) / 5, 10, 1), "食堂连廊");
+      this.currentScene = scene2;
       this.sceneMap = new Map();
       this.sceneMap[scene1.name] = scene1;
       this.sceneMap[scene2.name] = scene2;
-      console.log("scenes inited success");
+      this.sceneMap[scene3.name] = scene3;
+      this.sceneMap[scene4.name] = scene4;
+      this.sceneMap[scene5.name] = scene5;
     },
     rendererInit: function() {
       this.scene = new THREE.Scene();
       this.scene.updateMatrixWorld(true);
       this.camera = new THREE.PerspectiveCamera(
-        72,
+        100,
         window.innerWidth / window.innerHeight,
         0.01,
         1000
@@ -107,15 +100,17 @@ export default {
       this.orientH = new orientHandler();
       this.skyBoxGeom = new THREE.SphereGeometry(100, 100, 100);
       this.skyBoxGeom.scale(1, 1, -1);
-      console.log("SB1");
       this.loadScene(this.currentScene);
       this.scene.rotation.set(0, 0, 0);
-      this.renderer = new THREE.WebGLRenderer({ antialias: true });
+      this.renderer = new THREE.WebGLRenderer({
+        antialias: true,
+        precision: "highp"
+      });
       this.renderer.setSize(window.innerWidth, window.innerHeight);
+      this.renderer.setPixelRatio(window.devicePixelRatio);
       document
         .getElementById("sceneContainer")
         .appendChild(this.renderer.domElement);
-      console.log("RENDER INIT SUCCESS");
     },
     block() {
       while (!this.skyboxReady);
@@ -137,10 +132,7 @@ export default {
       });
       window.addEventListener(
         "deviceorientation",
-        () => {
-          alert("deviceorientation");
-        }
-        //this.onDeviceOrientation.bind(this)
+        this.onDeviceOrientation.bind(this)
       );
       window.addEventListener(
         "orientationchange",
@@ -225,22 +217,42 @@ export default {
       this.currentScreenOrientation = window.orientation;
     },
     jumpTo: function(e) {
-      console.log(e);
       this.unloadScene(this.currentScene);
       this.currentScene = this.sceneMap[e.detail.dest];
       this.loadScene(this.currentScene);
     },
+    getJumpButton: function(theta, row, h) {
+      let arr = "https://i.loli.net/2019/07/07/5d20d7e3486ca89261.png"; //"https://i.loli.net/2019/07/02/5d1b51a6c0c4a70528.png";
+      let tex = new THREE.TextureLoader().load(arr);
+      tex.magFilter = THREE.NearestFilter;
+      let planeObj = new THREE.Mesh(
+        new THREE.PlaneGeometry(2, 2, 10, 10),
+        new THREE.MeshBasicMaterial({
+          map: tex,
+          alphaTest: 0.5,
+          side: THREE.DoubleSide
+        })
+      );
+      planeObj.position.x = row * Math.cos(theta);
+      planeObj.position.y = h;
+      planeObj.position.z = row * Math.sin(theta);
+      planeObj.rotation.y = Math.PI / 2;
+      planeObj.rotation.y = Math.PI / 2 - theta;
+      return planeObj;
+    },
     loadScene: function(sc) {
-      this.loadBgImg(sc.bgSrc);
+      if (sc.bgPreSrc.length > 0) {
+        this.preLodaBgImg(sc.bgPreSrc);
+      } else {
+        this.loadBgImg(sc.bgSrc);
+      }
       for (let obj of sc.jumpObj) {
         this.scene.add(obj);
       }
       for (let obj of sc.descObj) {
-        console.log(obj);
         this.scene.add(obj);
       }
       for (let obj of sc.normalObj) {
-        console.log(obj);
         this.scene.add(obj);
       }
     },
@@ -256,20 +268,43 @@ export default {
       }
       this.unloadBgImg();
     },
-    loadBgImg: function(src) {
-      this.skyboxReady = false;
-      this.skyBoxTexture = new THREE.TextureLoader().load(src, () => {
-        console.log("LODOK");
-        this.skyboxReady = true;
+    preLodaBgImg: function(src) {
+      console.log("PRELOAD", src);
+      this.preSkyBoxTexture = new THREE.TextureLoader().load(src, () => {
+        this.preSkyBoxTexture.generateMipmaps = false;
+        this.preSkyBoxTexture.magFilter = THREE.LinearFilter;
+        this.preSkyBoxTexture.minFilter = THREE.LinearFilter;
         if (!(JSON.stringify(this.skyBoxMesh) === "{}")) {
           this.scene.remove(this.skyBoxMesh);
+        }
+        this.preSkyBoxMat = new THREE.MeshBasicMaterial({
+          map: this.preSkyBoxTexture
+        });
+        this.skyBoxMesh = new THREE.Mesh(this.skyBoxGeom, this.preSkyBoxMat);
+        this.scene.add(this.skyBoxMesh);
+        this.loadBgImg(this.currentScene.bgSrc);
+      });
+    },
+    loadBgImg: function(src) {
+      console.log("LOAD", src);
+      this.skyboxReady = false;
+      this.skyBoxTexture = new THREE.TextureLoader().load(src, () => {
+        this.skyboxReady = true;
+        this.skyBoxTexture.generateMipmaps = false;
+        this.skyBoxTexture.magFilter = THREE.LinearFilter;
+        this.skyBoxTexture.minFilter = THREE.LinearFilter;
+        if (!(JSON.stringify(this.skyBoxMesh) === "{}")) {
+          this.scene.remove(this.skyBoxMesh);
+        }
+        if (!(this.preSkyBoxTexture === null)) {
+          this.preSkyBoxMat.dispose();
+          this.preSkyBoxTexture.dispose();
         }
         this.skyBoxMat = new THREE.MeshBasicMaterial({
           map: this.skyBoxTexture
         });
         this.skyBoxMesh = new THREE.Mesh(this.skyBoxGeom, this.skyBoxMat);
         this.scene.add(this.skyBoxMesh);
-        console.log("LOAD SUCCESS");
       });
     },
     unloadBgImg: function() {
@@ -294,6 +329,9 @@ export default {
       this.camera.target.z = 500 * Math.sin(eularRadX) * Math.sin(eularRadY);
       this.camera.lookAt(this.camera.target);
       this.renderer.render(this.scene, this.camera);
+      //this.planeObj.position.z += 0.01;
+      //if (this.planeObj.position.z > Math.PI * 2) this.planeObj.position.z = 0;
+      //console.log(this.planeObj.rotation.x);
     },
 
     animate: function() {
@@ -305,7 +343,6 @@ export default {
     this.sceneObjInit();
     this.rendererInit();
     this.initEventListener();
-    console.log("EVENY LISTENER INIT SUCCESS");
     this.animate();
     //this.animate();
   }
--- src/components/scripts/scene.js
+++ src/components/scripts/scene.js
@@ -1,13 +1,14 @@
 //import * as THREE from "three";
 
 export default class sceneObj {
-  constructor(name, src) {
+  constructor(name, src, presrc = "") {
     this.name = name;
     this.active = false;
     this.jumpObj = [];
     this.descObj = [];
     this.normalObj = [];
     this.bgSrc = src;
+    this.bgPreSrc = presrc;
     this.texture = {};
   }
   addNormalObj(obj) {
@@ -15,6 +16,7 @@ export default class sceneObj {
   }
   addJumpObj(obj, dest) {
     obj.callbk = () => {
+      //alert("OK!");
       let e = new CustomEvent("jumpTo", {
         detail: { from: this.name, dest: dest },
         bubbles: false
@@ -25,8 +27,6 @@ export default class sceneObj {
   }
   addDescObj(obj, src) {
     obj.callbk = () => {
-      console.log("this is a description");
-      console.log(src);
       let audio = document.getElementById("desc");
       audio.src = src;
       audio.play();
